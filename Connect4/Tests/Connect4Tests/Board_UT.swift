c6d62cc3dc7703ba0c8acba3f483e78e7ba67ed7
Julien THEME <julien.theme@etu.uca.fr>
2023-01-22 14:15:33 +0100

    ✅ add test for Board's init method with grid

--- Connect4/Tests/Connect4Tests/Board_UT.swift
+++ Connect4/Tests/Connect4Tests/Board_UT.swift
@@ -27,4 +27,40 @@ final class Board_UT: XCTestCase {
         expect(iniBoardWithNbRows: -6, andNbColumns: 7, shouldBeNotNil: false)
         expect(iniBoardWithNbRows: 6, andNbColumns: 7, shouldBeNotNil: true)
     }
+
+    func testInitWithGrid() throws {
+
+        func expect(iniBoardWithGrid grid: [[Int?]], shouldBeNotNil notNil: Bool) {
+            let board = Board(grid: grid)
+            if !notNil {
+                XCTAssertNil(board)
+                return
+            }
+            XCTAssertNotNil(board)
+            XCTAssertEqual(grid.count, board?.nbRows)
+            XCTAssertEqual(grid[0].count, board?.nbColumns)
+            XCTAssertEqual(grid.count, board?.grid.count)
+            XCTAssertEqual(grid[0].count, board?.grid[0].count)
+
+            for i in 0..<grid.count {
+                XCTAssertEqual(grid[i].count, board?.grid[i].count)
+            }
+
+            for i in 0..<grid.count {
+                for j in 0..<grid[i].count {
+                    XCTAssertEqual(grid[i][j], board?.grid[i][j])
+                }
+            }
+        }
+
+        expect(iniBoardWithGrid: [], shouldBeNotNil: false)
+        expect(iniBoardWithGrid: [[]], shouldBeNotNil: false)
+        expect(iniBoardWithGrid: [[0]], shouldBeNotNil: true)
+        let grid1 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        expect(iniBoardWithGrid: grid1, shouldBeNotNil: true)
+        let grid2 = [[0, 0, 0], [0, 0, 0], [0, 0]]
+        expect(iniBoardWithGrid: grid2, shouldBeNotNil: false)
+        let grid3 = [[0, 0, 0], [0, 0, 0], [0, 0, 0, 0]]
+        expect(iniBoardWithGrid: grid3, shouldBeNotNil: false)
+    }
 }
